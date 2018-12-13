dc8222f9e93d7ad3207520f5686534b956b27d62
LooorTor <looortor@gmail.com>
2018-12-13 14:45:39 +0800

    feat: update mainet node

--- obsr-light-wallets-core/src/main/java/network/PeerGlobalData.java
+++ obsr-light-wallets-core/src/main/java/network/PeerGlobalData.java
@@ -11,7 +11,7 @@ public class PeerGlobalData {
 
     public static final String[] TRUSTED_TEST_NODES = new String[]{"testnet1.seeder.obsr.org"};
 
-    public static final String[] TRUSTED_NODES = new String[]{"main1.seeder.obsr.org"};
+    public static final String[] TRUSTED_NODES = new String[]{"node.obsr.org"};
 
     public static final List<PeerData> listTrustedHosts() {
         List<PeerData> list = new ArrayList<>();
